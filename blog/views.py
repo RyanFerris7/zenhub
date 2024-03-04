5ebe68311417002504a56325e3e0a00bb30ead74
Ryan Ferris <r.ferris137@gmail.com>
2024-03-04 21:26:20 +0000

    remove redundant code]

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-def home(request):
-
-    all_posts = Post.objects.all()
-    return render(request, 'index.html', {'post' : post})

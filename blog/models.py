b0e43c5d9590c3e1a978dfe46b7471000321f245
Ryan Ferris <r.ferris137@gmail.com>
2024-03-04 20:14:27 +0000

    add new model and view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model): 
+
+    class NewManager(models.Manager):
+
+        # The code below checks blog status and displays
+        # only published articles.
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  
+    publish = models.DateTimeField(default=timezone.now) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    objects = models.Manager() # Default manager
+    newmanager = NewManager() # Custom manager
+
+    # This code gives us the URL for unique pages.
+    # It takes the slug, and returns it. 
+    def get_absolute_url(self):
+        return reverse('blog:post_single',args=[self.slug])
+
+    # The code below orders posts by published date. Changed to 
+    # '-publish' to reverse order.
+    class Meta:
+        ordering = ('-publish',)
+
+    # The code below returns the correct title to posts. 
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def home(request):
+
+    all_posts = Post.newmanager.all()
+
+    return render(request, 'index.html', {'posts' : all_posts})
+
+# This code uses the slug tag to define a link to a page. 
+# It also checks post status. 
+
+def post_single(request, post):
+
+    post = get_object_or_404(Post, slug=post, status='published')
+
+    return render(request, 'single.html', {'post' : post})

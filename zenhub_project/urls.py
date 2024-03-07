7ef64b5f3d49fb0bbd837b9d047301988fd999da
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-03-07 11:55:16 +0000

    account and registration code added

--- zenhub_project/urls.py
+++ zenhub_project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView  # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+    path("", TemplateView.as_view(template_name="signhome.html"), name="home"),  # new
     # path('accounts/', include('accounts.urls')),
     # path('events/', include('events.urls')),
     # path('blog/', include('blog.urls')),

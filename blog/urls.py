491c6188c7c51fc21fe7ae68e6ff0fb96b1180fa
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-08 14:05:07 +0000

    blog comment display fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-# from .views import PostList
+from .views import PostList
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='homepage'),
+    path('', views.PostList.as_view(), name='blog'),
     path('<slug:post>/', views.post_detail, name="post_detail"), 
 ]

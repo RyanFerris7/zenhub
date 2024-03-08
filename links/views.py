664c24ea5e4ff1de4ab5ac220e1bf60200a004da
Ryan Ferris <r.ferris137@gmail.com>
2024-03-08 09:04:17 +0000

    functioning link in navbar events git com

--- links/views.py
+++ links/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import render, redirect
 from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'links/home.html')
 
 
 def team(request):

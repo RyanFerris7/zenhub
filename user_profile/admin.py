fb633d2f887937684045c1dee1fbb48c0efc9a23
Cian Thornhill <cianthornhill95@gmail.com>
2024-03-08 11:29:46 +0000

    edit user admin view to show booking

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import UserProfile
+from events.models import Booking
 
-# Register your models here.
+# Define inline admin model for Booking
+class BookingInline(admin.TabularInline):
+    model = Booking
+    extra = 0
+
+# Extend the UserAdmin
+class CustomUserAdmin(UserAdmin):
+    inlines = [BookingInline]
+
+# Register models
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(UserProfile)
